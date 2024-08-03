3342ca4ae1d3ba8e45298c55f510a2db39a17be4
Oonet96 <captaing96@gmail.com>
2024-08-03 19:38:00 +0900

    24.08.03 HT: 작업물 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">홈</router-link>
+    <router-link to='/book'>북마크</router-link>
+    <router-link to='/note'>정리노트</router-link>
+    <router-link to='/github'>작업한프로젝트</router-link>
+  </nav>
+  <router-view id="main_container"/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,7 +14,38 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2C3E50;
+}
+#main_container{
+  margin-top: 45px;
+}
+nav {
+  z-index : 1;
+  position: fixed;
+  width:100%;
+  height: 35px;
+  top:0px;
+  right:0px;
+  display: flex;
+  flex-direction:row;
+  background-color: white;
+  box-sizing: border-box;
+  padding:10px 20px;
+  box-shadow: 0 13px 27px -5px rgba(50,50,93,0.25),
+              0  8px 16px -8px rgba(0,0,0,0.3),
+              0 -6px 16px -6px rgba(0,0,0,0.25);
+  border-radius: 5px;
+}
+nav a {
+  text-decoration: none;
+  color : #0008FC;
+  /* text-transform:uppercase; */
+  font-size: 12px;
+  text-align:center;
+  font-weight: 600;
+}
+nav a:not(:last-child){
+  margin-right: 10px;
+  /* margin-bottom: 10px; */
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<script>
+import GitRepoView from './GitRepoView.vue'
+import BookmarkView from './BookmarkView.vue'
+import SummaryView from './SummaryView.vue'
+
+export default {
+  name: 'HomeView',
+  props: {
+    
+  },
+  components: {
+    // 추가적으로 사용할 컴포넌트들을 등록합니다.
+  },
+  data() {
+    return {
+      // 컴포넌트의 데이터를 초기화합니다.
+    };
+  },
+  watch: {
+    // sample1() {
+    //   console.log('');
+    // }
+    // 데이터를 감시하고 처리할 로직을 작성합니다.
+  },
+  computed: {
+    // sample2() {
+    //   return '';
+    // }
+    // 필요한 계산된 속성을 정의합니다.
+  },
+  methods: {
+    // sample3() {
+    //   return '';
+    // }
+    // 컴포넌트에서 사용할 메서드를 정의합니다.
+  },
+  setup() {
+    // Vue 3 Composition API의 setup 함수에서 추가적인 로직을 처리할 수 있습니다.
+  },
+  created() {
+    // 컴포넌트가 생성될 때 실행될 로직을 작성합니다.
+  },
+  mounted() {
+    // 컴포넌트가 DOM에 마운트된 직후 실행될 로직을 작성합니다.
+  },
+  unmounted() {
+    // 컴포넌트가 파괴되기 전 실행될 로직을 작성합니다.
+  }
+};
+</script>
+
+<template>
+  <div class="">
+    <div id="git_preview_container"></div>
+    <div id="bookmark_container"></div>
+    <div id="summary_list_container"></div>
+  </div>
+</template>
+
+<style scoped>
+/* 스타일을 추가하세요 */
+</style>
